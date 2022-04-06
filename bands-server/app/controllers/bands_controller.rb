1f07de09aaabd1fa7b6624b44a9ded5f58180f23
McCray <tubach75@hotmail.com>
2022-04-06 12:03:29 -0400

    added fast json gem to gemfile

--- bands-server/app/controllers/bands_controller.rb
+++ bands-server/app/controllers/bands_controller.rb
@@ -2,7 +2,8 @@ class BandsController < ApplicationController
 
     def index
         bands = Band.all
-        render json: bands.to_json(only: [:id, :name], include: [:players])
+        render json: bands.to_json(only: [:id, :name], include: {players: {except: [:created_at, :updated_at]}})
     end
+
 
 end
