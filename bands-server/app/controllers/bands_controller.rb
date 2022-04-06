a8bf7d2078d6d795507dcc30a1cfe63cbd955104
McCray <tubach75@hotmail.com>
2022-04-06 11:46:04 -0400

    removed created at and updated at from returned json

--- bands-server/app/controllers/bands_controller.rb
+++ bands-server/app/controllers/bands_controller.rb
@@ -2,7 +2,7 @@ class BandsController < ApplicationController
 
     def index
         bands = Band.all
-        render json: bands
+        render json: bands.to_json(only: [:id, :name], include: [:players])
     end
 
 end
--- bands-server/app/controllers/players_controller.rb
+++ bands-server/app/controllers/players_controller.rb
@@ -2,7 +2,7 @@ class PlayersController < ApplicationController
 
     def index
         players = Player.all
-        render json: players
+        render json: players.to_json(except: [:created_at, :updated_at])
     end
 
 end
