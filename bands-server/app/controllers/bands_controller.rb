b3211118b47eea99984941a1649bf83a5beb0c30
McCray <tubach75@hotmail.com>
2022-04-08 10:46:19 -0400

    added index html and js files to client side folder

--- bands-server/app/controllers/bands_controller.rb
+++ bands-server/app/controllers/bands_controller.rb
@@ -1,9 +1,24 @@
 class BandsController < ApplicationController
+    before_action :set_band, only: [:show, :update, :destroy]
 
     def index
         bands = Band.all
-        render json: BandSerializer.new(bands)
+        render json: BandSerializer.new(bands, {include: [:players]})
     end
 
+    def show
+        #band = Band.find(params[:id])
+        render json: @band
+    end
+
+    private
+
+    def set_band
+        @band = Band.find(params[:id])
+    end
+
+    def band_params
+        params.require(:band).permit(:name)
+    end
 
 end
