28cf181ca32b6806dfc7cb703f21893b728736ed
McCray <tubach75@hotmail.com>
2022-04-10 12:05:47 -0400

    configured App.js file

---
+++ bands-client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BandsList } from './features/bands/BandsList'
+
+export const App = () => {
+    return(
+        <>
+            <BandsList />
+        </>
+    )
+}
