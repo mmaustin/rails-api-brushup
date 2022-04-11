79dbcdc1cb78c3a1f2828b70745c7caf56af050b
McCray <tubach75@hotmail.com>
2022-04-11 12:34:21 -0400

    implemented thunk logic to retrieve bands from server

--- bands-client/src/features/bands/bandsSlice.js
+++ bands-client/src/features/bands/bandsSlice.js
@@ -1,6 +1,15 @@
-import { createSlice} from "@reduxjs/toolkit"; //nanoid
+import { createSlice, createAsyncThunk} from "@reduxjs/toolkit"; //nanoid
+import DataService from '../services/service'
 
 const initialState = []
+export const retrieveBands = createAsyncThunk(
+    "tutorials/retrieve",
+    async () => {
+      const res = await DataService.getAll();
+      return res.data;
+    }
+);
+
 
 const bandsSlice = createSlice({
     name: 'displayBands',
