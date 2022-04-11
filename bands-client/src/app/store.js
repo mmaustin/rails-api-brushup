2686d824548a93a03f5f7c3b5826b2ce418d3f1b
McCray <tubach75@hotmail.com>
2022-04-11 10:03:30 -0400

    configured store

--- bands-client/src/app/store.js
+++ bands-client/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import bandsSlice from '../features/bands/bandsSlice';
 
 export const store = configureStore({
   reducer: {
-    
+    displayBands: bandsSlice
   },
 });
--- bands-client/src/features/bands/BandsList.js
+++ bands-client/src/features/bands/BandsList.js
@@ -2,21 +2,12 @@ import React from 'react'
 
 export const BandsList = () => {
 
-    const fetchBands = () => {
-        fetch('http://localhost:3000/bands')
-        .then(resp=>resp.json())
-        .then(renderBands)
-      
-    }
 
-    const renderBands = bands => {
-        console.log(bands.data[0].id)
-    }
 
     return(
         <>
             <p>Can we even do this??? I don't think so.</p>
-            <button onClick={fetchBands}>Click This!</button>
+            <button>Click This!</button>
             
         </>
     )
