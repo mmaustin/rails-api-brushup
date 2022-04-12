97c3a7b68bfe3620b5cf0f4932ba3818556310f6
McCray <tubach75@hotmail.com>
2022-04-12 10:11:02 -0400

    troubleshooting issue with getting double pending and fulfilled messages in redux store

--- bands-client/src/features/bands/BandsList.js
+++ bands-client/src/features/bands/BandsList.js
@@ -1,12 +1,13 @@
 import React, {useEffect} from 'react'
-//import { selectAllBands } from './bandsSlice'
+import { selectAllBands } from './bandsSlice'
 import { retrieveBands } from './bandsSlice'
 import { useSelector, useDispatch } from 'react-redux'
 
 export const BandsList = () => {
     const dispatch = useDispatch()
 
-    //const bands = useSelector(selectAllBands)
+    const bands = useSelector(selectAllBands)
+    const art = bands[0].data[0].attributes.name
     const bandStatus = useSelector(state => state.displayBands.status)
 
     //the bands are being retrieved. i just have to figure out how to add them to the store.
@@ -19,7 +20,7 @@ export const BandsList = () => {
     return(
         <>
             <p>Can we even do this??? I don't think so.</p>
-            <p>What What!</p>
+            <p>{art}</p>
             <button>Click This!</button>
             
         </>
