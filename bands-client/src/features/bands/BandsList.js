04525e92fee7a0c54cd21ebbbb15dcf1e9093bfe
McCray <tubach75@hotmail.com>
2022-04-12 09:28:53 -0400

    added code to only fetch bands from api if status is idle

--- bands-client/src/features/bands/BandsList.js
+++ bands-client/src/features/bands/BandsList.js
@@ -1,18 +1,20 @@
 import React, {useEffect} from 'react'
-import { selectAllBands } from './bandsSlice'
+//import { selectAllBands } from './bandsSlice'
 import { retrieveBands } from './bandsSlice'
 import { useSelector, useDispatch } from 'react-redux'
 
 export const BandsList = () => {
     const dispatch = useDispatch()
 
-    const bands = useSelector(selectAllBands)
-    //const b = bands.map(b => b.toUpperCase())
+    //const bands = useSelector(selectAllBands)
+    const bandStatus = useSelector(state => state.displayBands.status)
 
     //the bands are being retrieved. i just have to figure out how to add them to the store.
     useEffect(() => {
+        if (bandStatus === 'idle'){
         dispatch(retrieveBands())
-    }, [bands, dispatch])
+        }
+    })
 
     return(
         <>
