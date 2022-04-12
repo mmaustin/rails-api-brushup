f968dd88b92b3e41bcaf58804aa3bede70191ae3
McCray <tubach75@hotmail.com>
2022-04-12 11:42:34 -0400

    added conditional statement to check for succeeded status

--- bands-client/src/features/bands/BandsList.js
+++ bands-client/src/features/bands/BandsList.js
@@ -7,20 +7,29 @@ export const BandsList = () => {
     const dispatch = useDispatch()
 
     const bands = useSelector(selectAllBands)
-    const art = bands[0].data[0].attributes.name
     const bandStatus = useSelector(state => state.displayBands.status)
 
-    //the bands are being retrieved. i just have to figure out how to add them to the store.
     useEffect(() => {
         if (bandStatus === 'idle'){
         dispatch(retrieveBands())
         }
-    })
+    }, [bandStatus, dispatch])
+
+    let content
+
+    if (bandStatus === 'succeeded'){
+        
+        const newBandsList = bands
+        content = newBandsList[0].included.map((b,i) => {
+            return <p key={i}>{b.attributes.name}</p>
+        })
+    }
+
 
     return(
         <>
             <p>Can we even do this??? I don't think so.</p>
-            <p>{art}</p>
+            <>{content}</>
             <button>Click This!</button>
             
         </>
