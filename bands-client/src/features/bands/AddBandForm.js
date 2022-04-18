29ebe0b276dd3e41c8be428cfce876527edd2ef7
McCray <tubach75@hotmail.com>
2022-04-18 11:34:19 -0400

    wrote the non-render code in add band form component

--- bands-client/src/features/bands/AddBandForm.js
+++ bands-client/src/features/bands/AddBandForm.js
@@ -1,9 +1,35 @@
 import React, {useState} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch} from 'react-redux'
 import { createBand } from './bandsSlice'
 
 export const AddBandForm = () => {
+    const [name, setName] = useState('');
+    const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
-    
+    const dispatch = useDispatch();
+
+    const onNameChanged = (e) => setName(e.target.value)
+
+    const canSave =
+    [name].every(Boolean) && addRequestStatus === 'idle'
+
+    const onSaveBandClicked = async () => {
+        if (canSave) {
+          try {
+            setAddRequestStatus('pending')
+            await dispatch(createBand({ name })).unwrap()
+            setName('')
+          } catch (err) {
+            console.error('Failed to save the post: ', err)
+          } finally {
+            setAddRequestStatus('idle')
+          }
+        }
+      }
+
+    return(
+        <>
+        </>
+    )
 
 }
