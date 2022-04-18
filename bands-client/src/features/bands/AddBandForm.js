5996605949424ffd067d900a395c29a27274f347
McCray <tubach75@hotmail.com>
2022-04-18 11:19:01 -0400

    created add band form component and import react, redux functions

---
+++ bands-client/src/features/bands/AddBandForm.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { createBand } from './bandsSlice'
+
+export const AddBandForm = () => {
+
+    
+
+}
