8c965f445c32536c4fd2d09e8e1cbc3d7dfd63ba
McCray <tubach75@hotmail.com>
2022-04-18 13:55:37 -0400

    added player form route to app component

--- bands-client/src/App.js
+++ bands-client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { BandsList } from './features/bands/BandsList'
 import { AddBandForm } from './features/bands/AddBandForm'
+import { AddPlayerForm } from './features/players/AddPlayerForm'
 
 const App = () => {
     return(
@@ -9,6 +10,7 @@ const App = () => {
             <Routes>
                 <Route exact path='/' element={<BandsList/>}/>
                 <Route path='/add-band' element={<AddBandForm/>}/>
+                <Route path='/add-player' element={<AddPlayerForm/>}/>
             </Routes>
         </Router>
     )
