ba0dbe5bd861b1a5f383336a73ede7d473f64583
McCray <tubach75@hotmail.com>
2022-04-18 11:49:56 -0400

    added two routes to app component

--- bands-client/src/App.js
+++ bands-client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { BandsList } from './features/bands/BandsList'
+import { AddBandForm } from './features/bands/AddBandForm'
 
 const App = () => {
     return(
-        <>
-            <BandsList />
-        </>
+        <Router>
+            <Routes>
+                <Route exact path='/' element={<BandsList/>}/>
+                <Route path='/add-band' element={<AddBandForm/>}/>
+            </Routes>
+        </Router>
     )
 }
 
