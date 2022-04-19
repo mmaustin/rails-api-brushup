e6732781c5174f7c2a4538ab14a1d79fc32820ae
McCray <tubach75@hotmail.com>
2022-04-19 12:29:49 -0400

    added retrieve band component to handling retrieving a single band

---
+++ bands-client/src/features/bands/RetrieveBand.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { selectAllBands } from './bandsSlice'
+import { retrieveBands } from './bandsSlice'
+import { useSelector, useDispatch } from 'react-redux'
+
+export const RetrieveBand = () => {
+    
+
+    return(
+        <>
+        </>
+    )
+}
