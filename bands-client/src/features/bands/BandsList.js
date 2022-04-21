9c3ba48c451befd109d245fb8492e69783437968
McCray <tubach75@hotmail.com>
2022-04-21 12:16:41 -0400

    reverted to old code, as troubleshooting did not fix the store issue

--- bands-client/src/features/bands/BandsList.js
+++ bands-client/src/features/bands/BandsList.js
@@ -2,7 +2,6 @@ import React, {useEffect} from 'react'
 import { selectAllBands } from './bandsSlice'
 import { retrieveBands } from './bandsSlice'
 import { useSelector, useDispatch } from 'react-redux'
-import { RetrieveBand } from './RetrieveBand'
 
 export const BandsList = () => {
     const dispatch = useDispatch()
@@ -35,7 +34,6 @@ export const BandsList = () => {
             <p>Can we even do this??? I don't think so.</p>
             <>{content}</>
             <button>Click This!</button>
-            <RetrieveBand b={bands}/>
         </>
     )
 }
--- bands-client/src/features/bands/RetrieveBand.js
+++ bands-client/src/features/bands/RetrieveBand.js
@@ -3,14 +3,13 @@ import { selectAllBands } from './bandsSlice'
 import { getBand } from './bandsSlice'
 import { useSelector, useDispatch } from 'react-redux'
 
-export const RetrieveBand = ({b}) => {
+export const RetrieveBand = () => {
     const dispatch = useDispatch()
     const [id, setId] = useState('')
 
     const bands = useSelector(selectAllBands)
     //console.log(bands)
     //const bandStatus = useSelector(state => state.displayBands)
-    console.log(b)
 
     const onChangeId = e => {setId(e.target.value);}
     const setToEmpty = () => {
