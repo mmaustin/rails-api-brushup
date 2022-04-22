bb03d1406740dd827a0305657869fdca97f2999f
McCray <tubach75@hotmail.com>
2022-04-22 09:26:07 -0400

    deleted blocked code from retrieve band component

--- bands-client/src/features/bands/RetrieveBand.js
+++ bands-client/src/features/bands/RetrieveBand.js
@@ -1,39 +1,15 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { selectAllBands } from './bandsSlice'
 import { getBand } from './bandsSlice'
 import { useSelector, useDispatch } from 'react-redux'
-import { retrieveBands } from './bandsSlice'
 
 export const RetrieveBand = () => {
     const dispatch = useDispatch()
     const [id, setId] = useState('')
 
-    //const bands = useSelector(selectAllBands)
-    //console.log(bands)
-    //const bandStatus = useSelector(state => state.displayBands)
 
     const bands = useSelector(selectAllBands)
-    /*const bandStatus = useSelector(state => state.displayBands.status)
 
-    useEffect(() => {
-        if (bandStatus === 'idle'){
-        dispatch(retrieveBands())
-        }
-    }, [bandStatus, dispatch])
-
-    let content
-
-    if (bandStatus === 'succeeded'){
-        const newBandsList = bands
-        content = newBandsList.map((b,i) => {
-            return <article key={i}>
-                <p key={i}>{b.name}</p>
-                 {b.players.map((p,i) => {
-                 return <p key={i}>{p.name}</p>
-                })}
-            </article>
-        })
-    }*/
 
     const onChangeId = e => {setId(e.target.value);}
     const setToEmpty = () => {
@@ -65,7 +41,6 @@ export const RetrieveBand = () => {
             <button onClick={getSingleBand}>Get Band</button>
             <div>
                 {band}
-                {/*{content}*/}
             </div>
         </div>
     )
