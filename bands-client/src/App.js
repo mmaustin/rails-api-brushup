15554a8e171b668e2b81bfefc723a98ae0c62b89
McCray <tubach75@hotmail.com>
2022-04-30 16:11:28 -0400

    switched to create root instead of reactdom in index.js file

--- bands-client/src/App.js
+++ bands-client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { BandsList } from './features/bands/BandsList'
 import { AddBandForm } from './features/bands/AddBandForm'
 import { AddPlayerForm } from './features/players/AddPlayerForm'
