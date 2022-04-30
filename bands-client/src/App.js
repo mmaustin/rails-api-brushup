4ce444e8dcd794e368cd81c031fb70e1bce59ba4
McCray <tubach75@hotmail.com>
2022-04-30 16:07:03 -0400

    removed deprecated switch and redirect imports from react router dom

--- bands-client/src/App.js
+++ bands-client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import { BandsList } from './features/bands/BandsList'
 import { AddBandForm } from './features/bands/AddBandForm'
 import { AddPlayerForm } from './features/players/AddPlayerForm'
@@ -10,18 +10,20 @@ const App = () => {
     return(
         <Router>
             <Routes>
-                <Route 
-                    exact path='/'
-                    element={<>
-                            <AddBandForm/>
-                            <BandsList/>
-                        </>
-                    }
-                 />
-                {/*<Route path='/add-band' element={<AddBandForm/>}/>*/}
-                <Route path='/add-player' element={<AddPlayerForm/>}/>
-                <Route path='/get-band' element={<RetrieveBand/>}/>
-                <Route path='/update-band' element={<UpdateBand/>}/>
+                
+                    <Route 
+                        exact path='/'
+                        element={<>
+                                <AddBandForm/>
+                                <BandsList/>
+                            </>
+                        }
+                    />
+                    {/*<Route path='/add-band' element={<AddBandForm/>}/>*/}
+                    <Route path='/add-player' element={<AddPlayerForm/>}/>
+                    <Route path='/get-band' element={<RetrieveBand/>}/>
+                    <Route path='/update-band' element={<UpdateBand/>}/>
+                
             </Routes>
         </Router>
     )
