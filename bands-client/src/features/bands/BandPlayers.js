af1470efa1db411fc44aa68fe229e197ff3b1d5e
McCray <tubach75@hotmail.com>
2022-05-03 08:21:12 -0400

    filled out presentational band players component

--- bands-client/src/features/bands/BandPlayers.js
+++ bands-client/src/features/bands/BandPlayers.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 export const BandPlayers = ({players}) => {
+
+    const playerList = players.map((p,i)=>{
+        return <p key={i}>{p.name}</p>
+    })
+
     return(
         <>
+            {playerList}
         </>
     )
 }
