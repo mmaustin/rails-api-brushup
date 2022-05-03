34d11ce7d0dab2b5344c709e96216a5e42d730f0
McCray <tubach75@hotmail.com>
2022-05-03 11:48:53 -0400

    added single band page component to test getting state

---
+++ bands-client/src/features/bands/SingleBandPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export const SingleBandPage = () => {
+
+
+    return(
+        <>
+            <p>Maybe it's this???</p>
+        </>
+    )
+}
