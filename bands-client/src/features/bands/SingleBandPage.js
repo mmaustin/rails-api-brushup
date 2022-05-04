640f59008377af8627dff490d0889fcd0b85d09c
McCray <tubach75@hotmail.com>
2022-05-04 10:40:35 -0400

    imported select all bands selector function to single band component

--- bands-client/src/features/bands/SingleBandPage.js
+++ bands-client/src/features/bands/SingleBandPage.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';//, useNavigation
-import { getBandById } from './bandsSlice'
+//import { getBandById } from './bandsSlice'
+import { selectAllBands } from './bandsSlice';
 
 export const SingleBandPage = () => {
     const params = useParams();
-    //console.log(params.bandId);
-    const band = useSelector(state => getBandById(state, params));
-    console.log(band)
+    const bandId = params.bandId;
+    const bands = useSelector(selectAllBands);
+    console.log(bands)
 
     return(
         <>
