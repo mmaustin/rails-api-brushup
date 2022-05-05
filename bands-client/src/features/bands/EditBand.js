c070f388a544b627a71479c7096d24405b2da1c1
McCray <tubach75@hotmail.com>
2022-05-05 10:10:56 -0400

    set up edit band component without the form and use of use state

--- bands-client/src/features/bands/EditBand.js
+++ bands-client/src/features/bands/EditBand.js
@@ -1,12 +1,18 @@
 import React, {useState} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate} from 'react-router-dom'
+import { useNavigate, useParams} from 'react-router-dom'
+import { getBandById } from './bandsSlice'
 
 export const EditBand = () => {
+    let navigate = useNavigate();
+    const params = useParams();
+    const bandId = parseInt(params.bandId);
+    const band = useSelector(state => getBandById(state, bandId))
 
 
     return(
         <>
+            <p>{band.name}</p>
         </>
     )
 }
