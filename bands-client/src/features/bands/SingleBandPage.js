48e129970de91123fac5bc04548bf75bdc30a2ca
McCray <tubach75@hotmail.com>
2022-05-05 09:34:11 -0400

    implemented use navigate to return to homepage if no band exists

--- bands-client/src/features/bands/SingleBandPage.js
+++ bands-client/src/features/bands/SingleBandPage.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom';//, useNavigation
+import { useParams, useNavigate } from 'react-router-dom';
 import { getBandById } from './bandsSlice'
 //import { selectAllBands } from './bandsSlice';
 
 export const SingleBandPage = () => {
+    let navigate = useNavigate();
     const params = useParams();
     const bandId = parseInt(params.bandId);
     console.log(bandId)
     //const bands = useSelector(selectAllBands);
     const band = useSelector(state => getBandById(state, bandId))
     console.log(band)
+
+    if(!band){
+        navigate('/');
+    }
 
     return(
         <>
