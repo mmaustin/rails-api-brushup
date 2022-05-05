c11055c4ebb98624a086e79596cd5e89e2d7cee5
McCray <tubach75@hotmail.com>
2022-05-05 09:16:53 -0400

    implemented get band by id selector function in single band component

--- bands-client/src/features/bands/SingleBandPage.js
+++ bands-client/src/features/bands/SingleBandPage.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';//, useNavigation
-//import { getBandById } from './bandsSlice'
-import { selectAllBands } from './bandsSlice';
+import { getBandById } from './bandsSlice'
+//import { selectAllBands } from './bandsSlice';
 
 export const SingleBandPage = () => {
     const params = useParams();
     const bandId = parseInt(params.bandId);
-    console.log(typeof bandId);
-    const bands = useSelector(selectAllBands);
-    console.log(bands)
-    const band = bands.find(band => band.id === bandId)
-    console.log(band)
+    //const bands = useSelector(selectAllBands);
+    const band = useSelector(state => getBandById(state, bandId))
     
 
     return(
