80c1e303846f3a00d4f654824adee5995724715a
McCray <tubach75@hotmail.com>
2022-05-07 12:06:43 -0400

    implemented delete band functionality in single band page component

--- bands-client/src/features/bands/SingleBandPage.js
+++ bands-client/src/features/bands/SingleBandPage.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { getBandById } from './bandsSlice'
 //import { selectAllBands } from './bandsSlice';
 import { deleteBand } from './bandsSlice';
 
 export const SingleBandPage = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const params = useParams();
     const bandId = parseInt(params.bandId);
-    //console.log(bandId)
-    //const bands = useSelector(selectAllBands);
+
     const band = useSelector(state => getBandById(state, bandId))
-    //console.log(band)
+
+    const deleteThisBand = () => {
+        dispatch(deleteBand({id: band.id}));
+        navigate('/');
+    }
 
     return(
         <>
             <p>{band.name}</p>
             <Link to={`/update-band/${band.id}`} >Edit Band</Link>
+            <button onClick={deleteThisBand}>Delete Band</button>
         </>
     )
 }
