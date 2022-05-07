410ebec91dd0e759c6821cb564bc99beea470608
McCray <tubach75@hotmail.com>
2022-05-07 12:13:04 -0400

    reimpleneted add band route in App component

--- bands-client/src/App.js
+++ bands-client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { BandsList } from './features/bands/BandsList'
-//import { AddBandForm } from './features/bands/AddBandForm'
+import { AddBandForm } from './features/bands/AddBandForm'
 import { AddPlayerForm } from './features/players/AddPlayerForm'
 //import { RetrieveBand } from './features/bands/RetrieveBand'
 //import { UpdateBand } from './features/bands/UpdateBand'
@@ -21,7 +21,7 @@ const App = () => {
                             </>
                         }
                     />
-                    {/*<Route path='/add-band' element={<AddBandForm/>}/>*/}
+                    <Route path='/add-band' element={<AddBandForm/>}/>
                     <Route exact path='/add-player' element={<AddPlayerForm/>}/>
                     <Route exact path='/get-band/:bandId' element={<SingleBandPage/>}/>
                     <Route exact path='/update-band/:bandId' element={<EditBand/>}/>
