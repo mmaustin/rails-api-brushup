c823491bddf9eab75f1654ec264f71f6b0751f1d
McCray <tubach75@hotmail.com>
2022-05-09 12:21:12 -0400

    configured players slice to retrieve all players

--- bands-client/src/features/players/playersSlice.js
+++ bands-client/src/features/players/playersSlice.js
@@ -1,7 +1,14 @@
 import { createSlice, createAsyncThunk} from "@reduxjs/toolkit"; //nanoid
 import DataServiceP from '../../services/servicePlayers'
 
-const initialState = {players: []}
+const initialState = {players: [], status: 'idle', error: null}
+export const retrievePlayers = createAsyncThunk(
+  "players/retrieve",
+  async () => {
+    const res = await DataServiceP.getAll();
+    return res.data;
+  }
+);
 export const createPlayer = createAsyncThunk(
     "players/create",
     async ({ name, instrument, band_id }) => {
@@ -19,6 +26,13 @@ export const createPlayer = createAsyncThunk(
 
     extraReducers(builder) {
       builder
+      .addCase(retrievePlayers.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(retrievePlayers.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.players = state.players.concat(action.payload)
+      })
       .addCase(createPlayer.fulfilled, (state, action) => {
         state.status = 'succeeded'
         state.players = state.players.concat(action.payload)
