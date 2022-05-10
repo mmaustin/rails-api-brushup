d586ed79ab338c8da92ba4f6fc25150406f54e77
McCray <tubach75@hotmail.com>
2022-05-10 10:10:54 -0400

    returned index code to utilization of reactDom

--- bands-client/src/index.js
+++ bands-client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-//import ReactDOM from 'react-dom'
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom'
+//import { createRoot } from 'react-dom/client';
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
@@ -9,7 +9,7 @@ import './index.css'
 //import {retrievePlayers} from './features/players/playersSlice'
 
 //store.dispatch(retrievePlayers());
-/*
+
 ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
@@ -18,9 +18,9 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('root')
   )
-*/
 
 
+/*
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
@@ -30,3 +30,4 @@ root.render(
   </Provider>
 </React.StrictMode>
 );
+*/
