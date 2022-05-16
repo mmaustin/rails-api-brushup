1ada537432c0c40370ccfae701468bc14d75bf90
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-16 10:08:08 -0400

    added user model and performed migration

--- bands-server/db/schema.rb
+++ bands-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_05_164905) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_16_140346) do
   create_table "bands", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_05_164905) do
     t.string "name"
     t.string "instrument"
     t.integer "band_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "emai"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
