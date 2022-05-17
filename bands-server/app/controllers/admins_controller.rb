e1f1f19d03398b058e6ec2e822789263300a7c78
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-17 12:23:29 -0400

    built out the remaining routes for the admin resource

--- bands-server/app/controllers/admins_controller.rb
+++ bands-server/app/controllers/admins_controller.rb
@@ -1,8 +1,44 @@
 class AdminsController < ApplicationController
 
+    before_action :set_admin, only: [:show, :update, :destroy]
+
     def index
         admins = Admin.all
         render json: admins.to_json(only: [:id, :username, :email])
+    end
+
+    def show
+        #band = Band.find(params[:id])
+        render json: @admin.to_json(only: [:id, :username, :emial]) #, :include => {:players => {only: [:name, :instrument, :band_id]}})
+    end
+
+    def create
+        @admin = Admin.new(admin_params)
+    
+        if @admin.save
+          render json: @admin, status: :created, location: @admin
+        else
+          render json: @admin.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @admin.update(admin_params)
+        render json: @admin
+    end
+
+    def destroy
+        @admin.destroy
+    end   
+
+    private
+
+    def set_admin
+        @admin = Admin.find(params[:id])
+    end
+
+    def admin_params
+        params.require(:admin).permit(:username, :email, :password)
     end
 
 end
