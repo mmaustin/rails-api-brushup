6a5468e2534565b7b6ede624e804a9cd58d8653f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-17 11:14:42 -0400

    limited attributes displayed in json

--- bands-server/app/controllers/admins_controller.rb
+++ bands-server/app/controllers/admins_controller.rb
@@ -2,7 +2,7 @@ class AdminsController < ApplicationController
 
     def index
         admins = Admin.all
-        render json: admins.to_json()
+        render json: admins.to_json(only: [:id, :username, :email])
     end
 
 end
