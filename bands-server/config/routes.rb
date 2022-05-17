1ccb5ad3fe5d7899a3c487ce330b3a5ce31c9567
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-17 10:59:43 -0400

    added a user resource in rails

--- bands-server/config/routes.rb
+++ bands-server/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :admins
   resources :users
   resources :players
   resources :bands
--- bands-server/db/schema.rb
+++ bands-server/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_16_140346) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_145404) do
+  create_table "admins", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "bands", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
