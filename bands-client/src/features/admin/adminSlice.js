f8dabc6647d5a5c7f36241bee606e25e103d01cb
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-18 10:33:09 -0400

    created initial admin state and retrieve admins thunk

--- bands-client/src/features/admin/adminSlice.js
+++ bands-client/src/features/admin/adminSlice.js
@@ -1,2 +1,11 @@
 import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import DataServiceA from '../../services/serviceAdmin';
+
+const initialState = {admins: [], status: 'idle', error: null,}
+export const retrieveAdmins = createAsyncThunk(
+  "admins/retrieve",
+  async () => {
+    const res = await DataServiceA.getAll();
+    return res.data;
+  }
+);
