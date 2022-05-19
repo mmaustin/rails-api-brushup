d1f36d0013c06a62373f882578b45132b6dfb0f3
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-19 10:08:13 -0400

    implemented use effect to add admins to state

--- bands-client/src/features/admin/CreateAdmin.js
+++ bands-client/src/features/admin/CreateAdmin.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react';
-import { useDispatch} from 'react-redux';
+import React, {useEffect} from 'react';
+import { useSelector, useDispatch} from 'react-redux';
+import { retrieveAdmins } from './adminSlice';
 
 export const CreateAdmin = () => {
+    const dispatch = useDispatch();
+    const adminStatus = useSelector(state => state.admin.status)
 
-
+    useEffect(() => {
+        if (adminStatus === 'idle'){
+        dispatch(retrieveAdmins())
+        }
+    }, [adminStatus, dispatch])
 
     return(
         <>
