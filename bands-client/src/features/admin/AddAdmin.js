63bb9ee56ac3c684bdfb335f04d92485520a3276
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-19 11:45:32 -0400

    added the onSaveAdmin function to add admin component

--- bands-client/src/features/admin/AddAdmin.js
+++ bands-client/src/features/admin/AddAdmin.js
@@ -10,8 +10,28 @@ export const AddAdmin =  () => {
     const [password, setPassword] = useState('');
     const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
+    const onUsernameChanged = (e) => setUsername(e.target.value);
+    const onEmailChanged = (e) => setEmail(e.target.value);
+    const onPasswordChanged = (e) => setPassword(e.target.value);
 
+    const canSave =
+    [username].every(Boolean) && [email].every(Boolean) && [password].every(Boolean) && addRequestStatus === 'idle'
 
+    const onSaveAdmin = async () => {
+        if (canSave) {
+          try {
+            setAddRequestStatus('pending')
+            await dispatch(createAdmin({ admin: {username: username, email: email, password: password} })).unwrap()
+            setUsername('');
+            setEmail('');
+            setPassword('');
+          } catch (err) {
+            console.error('Failed to save the admin: ', err)
+          } finally {
+            setAddRequestStatus('idle')
+          }
+        }
+      }
 
     return(
         <section>
