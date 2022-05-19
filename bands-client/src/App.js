a838df4f0eb9a51fc3ce85349f871369c795f832
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-19 09:49:01 -0400

    placed create admin component as the app's homepage

--- bands-client/src/App.js
+++ bands-client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { CreateAdmin } from './features/admin/CreateAdmin'
 import { BandsList } from './features/bands/BandsList'
 import { AddBandForm } from './features/bands/AddBandForm'
 import { AddPlayerForm } from './features/players/AddPlayerForm'
@@ -13,16 +14,9 @@ const App = () => {
         <Router>
             <Navbar/>
                 <Routes>
-                    
-                    <Route 
-                        exact path='/band-list'
-                        element={<>
-                                {/*<AddBandForm/>*/}
-                                <BandsList/>
-                            </>
-                        }
-                    />
-                    <Route path='/add-band' element={<AddBandForm/>}/>
+                    <Route exact path='/' element={<CreateAdmin/>} />
+                    <Route exact path='/band-list' element={<> {/*<AddBandForm/>*/} <BandsList/> </>}/>
+                    <Route exact path='/add-band' element={<AddBandForm/>}/>
                     <Route exact path='/add-player' element={<AddPlayerForm/>}/>
                     <Route exact path='/get-band/:bandId' element={<SingleBandPage/>}/>
                     <Route exact path='/update-band/:bandId' element={<EditBand/>}/>
--- bands-client/src/features/admin/CreateAdmin.js
+++ bands-client/src/features/admin/CreateAdmin.js
@@ -7,6 +7,7 @@ export const CreateAdmin = () => {
 
     return(
         <>
+            <p>HomePage!</p>
         </>
     )
 };
