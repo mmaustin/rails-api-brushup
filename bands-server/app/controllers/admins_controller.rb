ee733e03cea1a196615e5c439b35181790f41ee4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-21 12:38:21 -0400

    return @ instance method symbol to admin controller

--- bands-server/app/controllers/admins_controller.rb
+++ bands-server/app/controllers/admins_controller.rb
@@ -9,32 +9,32 @@ class AdminsController < ApplicationController
 
     def show
         #band = Band.find(params[:id])
-        render json: admin.to_json(only: [:id, :username, :email]) #, :include => {:players => {only: [:name, :instrument, :band_id]}})
+        render json: @admin.to_json(only: [:id, :username, :email]) #, :include => {:players => {only: [:name, :instrument, :band_id]}})
     end
 
     def create
-        admin = Admin.new(admin_params)
+        @admin = Admin.new(admin_params)
     
         if @admin.save
-          render json: admin, status: :created, location: admin
+          render json: @admin, status: :created, location: @admin
         else
-          render json: admin.errors, status: :unprocessable_entity
+          render json: @admin.errors, status: :unprocessable_entity
         end
     end
 
     def update
-        admin.update(admin_params)
-        render json: admin
+        @admin.update(admin_params)
+        render json: @admin
     end
 
     def destroy
-        admin.destroy
+        @admin.destroy
     end   
 
     private
 
     def set_admin
-        admin = Admin.find(params[:id])
+        @admin = Admin.find(params[:id])
     end
 
     def admin_params
