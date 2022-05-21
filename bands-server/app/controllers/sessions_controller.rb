4e966acd1820863b8d3a20cd3992bf1a8a885d4a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-21 14:21:19 -0400

    built out create and destroy routes in sessions controller

--- bands-server/app/controllers/sessions_controller.rb
+++ bands-server/app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
   def create
+    admin = Admin.find_by(username: params[:username])
+    if admin&.authenticate(params[:password])
+      session[:admin_id] = admin.id
+      render json: admin, status: :ok
+    else
+      render json: "Invalid Credentials", status: :unauthorized
+    end    
   end
 
   def destroy
+    def destroy
+      session.delete :admin_id
+    end
   end
 end
