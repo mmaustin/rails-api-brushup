748d3ea41c003736ff9b9bb7cd29d1d884bd2936
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-22 10:19:31 -0400

    built out admin session compoent

--- bands-client/src/features/admin/AdminSession.js
+++ bands-client/src/features/admin/AdminSession.js
@@ -4,10 +4,23 @@ import { getAdmin } from './adminSlice';
 
 export const AdminSession = () => {
 
+    const dispatch = useDispatch();
+    const admin = useSelector(state => state.admin.admins);
+    const adminStatus = useSelector(state => state.admin.status);
+    const [currentAdmin, setCurrentAdmin] = useState(null);
 
+    useEffect(() => {
+        dispatch(getAdmin())
+    }, [adminStatus, dispatch])
+
+
+    if(adminStatus === 'succeeded' || adminStatus === 'failed'){
+        setCurrentAdmin(admin);
+    }
     
     return(
         <>
+            {currentAdmin.length > 0 ? <p>There was one.</p> : <p>There were none.</p>}
         </>
     )
 }
