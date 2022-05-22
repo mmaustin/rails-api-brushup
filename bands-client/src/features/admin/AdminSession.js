eb4e53078a3b7e3ff05d1f291605d1daf43bbb79
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-22 09:51:09 -0400

    built out shell of admin session component

---
+++ bands-client/src/features/admin/AdminSession.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAdmin } from './adminSlice';
+
+export const AdminSession = () => {
+
+
+    
+    return(
+        <>
+        </>
+    )
+}
