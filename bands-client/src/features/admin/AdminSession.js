bd95394c8448c63f3fa815adfb7d228093f3c2f1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-25 10:20:06 -0400

    set current admin inside of use effect to prevent infinite loop

--- bands-client/src/features/admin/AdminSession.js
+++ bands-client/src/features/admin/AdminSession.js
@@ -7,21 +7,26 @@ export const AdminSession = () => {
     const dispatch = useDispatch();
     const admin = useSelector(state => state.admin.admins);
     const adminStatus = useSelector(state => state.admin.status);
-    //const [currentAdmin, setCurrentAdmin] = useState(null);
+    const [currentAdmin, setCurrentAdmin] = useState(null);
     //const [isAuthenticated, setIsAuthenticated] = useState(false);
     //console.log(admin)
     useEffect(() => {
         if(adminStatus === 'idle'){
             dispatch(getAdmin());
+            setCurrentAdmin(admin);
         }
-    })
+    }, [currentAdmin])
+
+    let a;
+    //setCurrentAdmin(admin);
 
 
-/*    if(adminStatus === 'succeeded'){
-        setCurrentAdmin(admin);
-       // setIsAuthenticated(true);
+    if(adminStatus === 'succeeded'){
+        a = admin.id;
+        //setCurrentAdmin(admin);
+       //setIsAuthenticated(true);
     }
-*/
+
 
 /*    if (!isAuthenticated) {
         return <div></div>;
@@ -32,6 +37,7 @@ export const AdminSession = () => {
     return(
         <>
            {admin ? <p>{admin.username}</p> : <p>There were none.</p>}
+           {a}
         </>
     )
 }
